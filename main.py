309b9ef9956ed16971e3841c5f6322797fd6161f
dollav <146855949+dollav@users.noreply.github.com>
2023-11-08 11:07:31 -0500

    Error handling

--- main.py
+++ main.py
@@ -2,36 +2,61 @@ import requests as reqs
 from kubernetes import client, config
 import json
 import os
+import time 
+import sys
 
-#globals
-# Configs can be set in Configuration class directly or using helper utility
-APIKEY = "Token a2de3794-00e4-49c8-8736-b37b3a3d25ca"
-orgId = "8a6554e2-1bdf-42a5-b21c-01f63d5ae7cd"
-integrationId= "2660c898-da9e-4c5c-9a81-443bd7ddd5e2"
-snykAPIVersion = "2023-11-06~beta"
+#Globals, probably worth changing these to environment variables or mounting them from a configmap
 
 
 #Scan any missing images, 'images' should be a iterable list
 #We can modify the arguments of Snyk Container to include tags which can then be used to create project views or import metadata from the pods if needed
 def scanMissingImages(images):
+    splitKey = APIKEY.split()
+
+    cmd = '/usr/local/bin/snyk auth {}'.format(splitKey[1])
+    os.system(cmd)
     for missingImage in images:
+
+        #cmd = '/usr/app/sec/snyk container monitor {} --org={} --tags=kubernetes=monitored'.format(missingImage, orgId)
+        print("Scanning {}").format(missingImage)
         cmd = '/usr/local/bin/snyk container monitor {} --org={} --tags=kubernetes=monitored'.format(missingImage, orgId)
         os.system(cmd)
 
 def deleteNonRunningTargets():
-    #will need to retry for when snyk UI dies
-    containerImageURL = "https://api.snyk.io/rest/orgs/{}/container_images?version={}".format(orgId, snykAPIVersion)
-    response = reqs.get(containerImageURL, headers={'Authorization': '{}'.format(APIKEY)})
-    containerResponseJSON = response.json()
 
-    allTargetsUrl = "https://api.snyk.io/rest/orgs/{}/targets?version={}".format(orgId, snykAPIVersion)
-    response = reqs.get(allTargetsUrl, headers={'Authorization': '{}'.format(APIKEY)})
-    targetResponseJSON = response.json()
+    #May be worth building in a retry mechanism instead of just failing
+    #Also both endpoints only cover the case where there is no nextPage key, adding in 'while next page != empty' will probably be nessisary for larger datasets
+    try:
+        containerImageURL = "https://api.snyk.io/rest/orgs/{}/container_images?version={}".format(orgId, snykAPIVersion)
+        containerResponse = reqs.get(containerImageURL, headers={'Authorization': '{}'.format(APIKEY)})
+        containerResponseJSON = containerResponse.json()
+        containerResponse.raise_for_status()
+    except reqs.HTTPError as ex:
+        print("ERROR: some error occured dumping target JSON {}".format(containerResponseJSON))
+        raise ex
+    except reqs.Timeout:
+        print("ERROR: Request to the container_images endpoint timed out, returning without completing")
+        raise ex
+
+    try:
+        allTargetsUrl = "https://api.snyk.io/rest/orgs/{}/targets?version={}".format(orgId, snykAPIVersion)
+        targetResponse = reqs.get(allTargetsUrl, headers={'Authorization': '{}'.format(APIKEY)})
+        targetResponseJSON = targetResponse.json()
+        targetResponse.raise_for_status()
+        
+    except reqs.HTTPError as ex:
+        print("ERROR: some error occured dumping target JSON {}".format(targetResponseJSON))
+        raise ex
+    except reqs.Timeout:
+        print("ERROR: Request to the Targets endpoint timed out, returning without completing")
+        raise ex        
+
+
 
     for containerImage in containerResponseJSON['data']:
         image = containerImage['attributes']['names'][0]
 
-        #image that is not running on the cluster
+        #image that is not running on the cluster   
         if image not in allRunningPods:
 
             #TODO: change the split to replace for '_', depending on the workflow it may make more sense to create targets with <image>_<version>
@@ -47,14 +72,16 @@ def deleteNonRunningTargets():
                         print("Some issue deleting targetID {}, based off image {}. Response code: {}".format(target['id'], imageTagStripped, deleteResp.status_code))
 
 
-
-
-
-
-#Load Kubeconfig for interacting with the K8s API. Load in K8s api V1 to query pods.
-config.load_incluster_config() #this loads in cluster config, use when deploying pods to k8s
-#config.load_kube_config() #local environment
+#Load Kubeconfig for interacting with the K8s API. Load in K8s api V1 to query pods. 
+if os.getenv('KUBERNETES_SERVICE_HOST'):
+    print("KUBERNETES_SERVICE_HOST detected, atempting to load in pod config... ")
+    config.load_incluster_config() 
+else:
+    print("KUBERNETES_SERVICE_HOST is not set, loading kubeconfig from localhost...")
+    config.load_kube_config()
 v1 = client.CoreV1Api()
+
+#vars for later logic
 allRunningPods = []
 needsToBeScanned = []
 
@@ -68,9 +95,15 @@ for pod in v1.list_pod_for_all_namespaces().items:
         #will need to add some retry logic incase the connection dies
         URL = "https://api.snyk.io/rest/orgs/{}/container_images?names={}&version={}".format(orgId, image, snykAPIVersion)
         
-        response = reqs.get(URL, headers={'Authorization': '{}'.format(APIKEY)})
-        responseJSON = response.json()
-
+        try:
+            response = reqs.get(URL, headers={'Authorization': '{}'.format(APIKEY)})
+            responseJSON = response.json()
+        except reqs.HTTPError as ex:
+            print("ERROR: Some error has occured, dumping response JSON: {}".format(responseJSON))
+            raise ex
+        except reqs.Timeout:
+            print("ERROR: Request to the container_images endpoint timed out for image {}".format(image))
+            
         #These are running on the API server but do not exist in Snyk
         #The None only works if the image has NEVER been scanned, after it has been scanned it we keep some data around
         if responseJSON.get('data') == None or 'data' not in responseJSON['data'][0]['relationships']['image_target_refs']:
@@ -78,7 +111,16 @@ for pod in v1.list_pod_for_all_namespaces().items:
 
 
 #Do the work we have set out to do o7
-scanMissingImages(needsToBeScanned)
+if len(needsToBeScanned) != 0:
+    scanMissingImages(needsToBeScanned)
+else:
+    print("All images on the cluster are accounted for, skipping scanning function")
+
+#For some reason it seemed like there was an issue with data being present if there was no delay
+#Might be safe to remove but for now I am just letting Snyk do some work in the background to ensure everyhting is
+#where we are expecting it to be
+time.sleep(5)
 deleteNonRunningTargets()
 
-    
+#clean exit when
+sys.exit()
